8e13181cf76423657a5d218359b192069b63e39a
adurlea <adrian.durlea@gmail.com>
2019-12-10 18:03:33 +0100

    TP4 implementations: - added two-way binding to new user form

--- src/app/user-add/user-add.component.ts
+++ src/app/user-add/user-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IUserNew } from '../user/user-new';
 
 @Component({
   selector: 'app-user-add',
@@ -6,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-add.component.css']
 })
 export class UserAddComponent implements OnInit {
-
+  newUser: IUserNew = {
+    name: 'Ausy',
+    age: 35,
+    job: 'ARC',
+    rating: 4,
+    jee: true,
+    java: true,
+    soa: false,
+    javascript: false,
+    angular: true,
+    junit: false,
+    selenium: false,
+    squash: false,
+    robot: false,
+    cpp: false,
+    net: false,
+    scrum: false,
+    kanban: false
+  };
   constructor() { }
 
   ngOnInit() {
